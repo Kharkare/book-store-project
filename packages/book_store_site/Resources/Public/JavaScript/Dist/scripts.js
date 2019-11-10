fd410776f60bb38b4ebbec066a766edd98877c22
Kunal Harkare <kunal.harkare@hof-university.de>
2019-11-10 19:59:53 +0100

    Integrated bootstrap site package extension

---
+++ packages/book_store_site/Resources/Public/JavaScript/Dist/scripts.js
@@ -0,0 +1,6 @@
+/*!
+ * Book Store Site v1.0.0 (https://www.hof-university.de)
+ * Copyright 2017-2019 Kunal Harkare
+ * Licensed under the GPL-2.0-or-later license
+ */
+console.log("WE LOVE TYPO3");
